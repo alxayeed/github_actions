099f63626bf19644a6b5c54221265c42cb1de681
alsayeed <alxayeed@gmail.com>
2023-07-08 11:03:14 +0600

    ui updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: const Text("Github Actions"),),
+        body: const Center(
+          child: Text('Build and Release APK '),
         ),
       ),
     );
